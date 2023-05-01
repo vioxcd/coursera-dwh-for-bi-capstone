cfcf5dc8e31e93063c2c68aa0e09e8d48fb07de3
RuslanUchan <ruslanuchan97@gmail.com>
2023-05-01 20:34:01 +0700

    create staging file for change data filled with basic transformation and JOIN on essential columns

--- implementation/integration/cpi_card/models/dw/dw_stg_job_change_data.sql
+++ implementation/integration/cpi_card/models/dw/dw_stg_job_change_data.sql
@@ -1,44 +1,5 @@
 
 WITH
-validation_rule_reject_null_values AS (
-  SELECT *
-  FROM {{ source('cpi_card', 'w_job_change_data') }}
-  WHERE
-    contract_date IS NOT NULL
-    AND sales_agent_id IS NOT NULL
-    AND sales_class_id IS NOT NULL
-    AND location_id IS NOT NULL
-    AND cust_id_ordered_by IS NOT NULL
-    AND date_promised IS NOT NULL
-    AND date_ship_by IS NOT NULL
-    AND number_of_subjobs IS NOT NULL
-    AND unit_price IS NOT NULL
-    AND quantity_ordered IS NOT NULL
-    AND quote_qty IS NOT NULL
-    AND lead_id IS NOT NULL
-),
-
-validation_rule_reject_invalid_fk_references AS (
-  SELECT *
-  FROM validation_rule_reject_null_values
-  WHERE
-    location_id IN (
-      SELECT location_id FROM {{ source('cpi_card', 'w_location_d') }}
-    )
-    AND
-    cust_id_ordered_by IN (
-      SELECT cust_key FROM {{ source('cpi_card', 'w_customer_d') }}
-    )
-    AND
-    sales_agent_id IN (
-      SELECT sales_agent_id FROM {{ source('cpi_card', 'w_sales_agent_d') }}
-    )
-    AND
-    sales_class_id IN (
-      SELECT sales_class_id FROM {{ source('cpi_card', 'w_sales_class_d') }}
-    )
-),
-
 create_date_fk_from_date_format AS (
   SELECT
     change_id,
@@ -54,24 +15,7 @@ create_date_fk_from_date_format AS (
     quantity_ordered,
     quote_qty,
     lead_id
-  FROM validation_rule_reject_invalid_fk_references
-),
-
-validation_rule_reject_invalid_date_references AS (
-  SELECT *
-  FROM create_date_fk_from_date_format
-  WHERE
-    contract_date IN (
-      SELECT time_id FROM {{ source('cpi_card', 'w_time_d') }}
-    )
-    AND
-    date_promised IN (
-      SELECT time_id FROM {{ source('cpi_card', 'w_time_d') }}
-    )
-    AND
-    date_ship_by IN (
-      SELECT time_id FROM {{ source('cpi_card', 'w_time_d') }}
-    )
+  FROM {{ source('cpi_card', 'w_job_change_data') }}
 ),
 
 business_day_numbering AS (
@@ -81,30 +25,25 @@ business_day_numbering AS (
   FROM {{ source('cpi_card', 'w_time_d') }}
 ),
 
-validation_rule_reject_invalid_business_day_differences AS (
+extra_date_number_columns AS (
   SELECT
-    v.*
-  FROM validation_rule_reject_invalid_date_references v
-    INNER JOIN business_day_numbering bd_contract
+    v.*,
+    bd_contract.business_day_number AS contract_business_day_number,
+    bd_promised.business_day_number AS promised_business_day_number,
+    bd_ship.business_day_number AS ship_by_business_day_number,
+    bd_created_date.business_day_number AS created_date_business_day_number
+  FROM create_date_fk_from_date_format v
+    LEFT JOIN {{ source('cpi_card', 'w_lead_f') }} l
+      USING (lead_id)
+    LEFT JOIN business_day_numbering bd_contract
       ON v.contract_date = bd_contract.time_id
-    INNER JOIN business_day_numbering bd_promised
+    LEFT JOIN business_day_numbering bd_promised
       ON v.date_promised = bd_promised.time_id
-    INNER JOIN business_day_numbering bd_ship
+    LEFT JOIN business_day_numbering bd_ship
       ON v.date_ship_by = bd_ship.time_id
-  WHERE
-    bd_promised.business_day_number - bd_contract.business_day_number BETWEEN 14 AND 30
-    AND
-    bd_promised.business_day_number - bd_ship.business_day_number BETWEEN 2 AND 7
-),
-
-validation_rule_reject_invalid_lead_id_references AS (
-  SELECT *
-  FROM validation_rule_reject_invalid_business_day_differences
-  WHERE
-    lead_id IN (
-      SELECT lead_id FROM {{ source('cpi_card', 'w_lead_f') }}
-    )
+    LEFT JOIN business_day_numbering bd_created_date
+      ON l.created_date = bd_created_date.time_id
 )
 
 SELECT *
-FROM validation_rule_reject_invalid_lead_id_references 
+FROM extra_date_number_columns
