676562abfcd3fcdff538e7d42a58aef64497b2bf
RuslanUchan <ruslanuchan97@gmail.com>
2023-06-17 19:07:48 +0700

    include number of jobs booked for dashboard 1

--- dashboard/viz-queries/2-lead-generation-by-location-and-agent.sql
+++ dashboard/viz-queries/2-lead-generation-by-location-and-agent.sql
@@ -1,15 +1,17 @@
 select
-	w_location_d.location_id
-	,w_location_d.location_name
-	,w_sales_agent_d.sales_agent_id
-	,w_sales_agent_d.sales_agent_name
-	,round(avg(dw_lead_f.quote_price), 2) as avg_quote_price
-	,sum(dw_lead_f.quote_qty) as total_quote_qty
+    w_location_d.location_id
+     ,w_location_d.location_name
+     ,w_sales_agent_d.sales_agent_id
+     ,w_sales_agent_d.sales_agent_name
+     ,round(avg(dw_lead_f.quote_price), 2) as avg_quote_price
+     ,sum(dw_lead_f.quote_qty) as total_quote_qty
+     ,count(1) as total_jobs_booked
 from dw_lead_f
-	inner join w_sales_agent_d
-		on w_sales_agent_d.sales_agent_id = dw_lead_f.sales_agent_id
-	inner join w_sales_class_d
-		on w_sales_class_d.sales_class_id = dw_lead_f.sales_class_id
-	inner join w_location_d
-		on w_location_d.location_id = dw_lead_f.location_id
+         inner join w_sales_agent_d
+                    on w_sales_agent_d.sales_agent_id = dw_lead_f.sales_agent_id
+         inner join w_sales_class_d
+                    on w_sales_class_d.sales_class_id = dw_lead_f.sales_class_id
+         inner join w_location_d
+                    on w_location_d.location_id = dw_lead_f.location_id
 group by 1, 2, 3, 4
+order by 1, 3
